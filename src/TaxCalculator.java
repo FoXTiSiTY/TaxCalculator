0c781f92408fb59610e56064aa0ea4e83dd0a8c6
Foxtisity <kentbrandon1@gmail.com>
2017-03-14 22:04:10 +0200

    First commit

---
+++ src/TaxCalculator.java
@@ -0,0 +1,14 @@
+
+public class TaxCalculator
+{
+	public TaxCalculator()
+	{
+		TaxTable table = new TaxTable(2018, 25);
+		System.out.println(table.calculateTax(16000.00)); 
+	}
+	
+	public static void main(String[] args)
+	{
+		TaxCalculator calc = new TaxCalculator();
+	}
+}
